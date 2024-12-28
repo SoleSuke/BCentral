ce7a70aec2668c3e30b38fffbba5999b28069594
Soledad Fuica <soledad.fuica@gmail.com>
2024-12-28 20:52:43 -0300

    Added controller and few changes in pom.xml

---
+++ src/main/java/com/project/task/controller/controlMSJ.java
@@ -0,0 +1,18 @@
+package com.project.task.controller;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import org.springframework.web.bind.annotation.
+
+import com.project.task.model.msjJSON;
+import com.project.task.service.MessageService;
+
+
