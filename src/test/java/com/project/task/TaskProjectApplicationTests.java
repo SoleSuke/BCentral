29b9966089038b8e367750e8bbdb605eca25e852
Soledad Fuica <soledad.fuica@gmail.com>
2024-12-31 14:27:14 -0300

    Removed unused Exception generic class, added cleanup hashmap for new tests

--- src/test/java/com/project/task/TaskProjectApplicationTests.java
+++ src/test/java/com/project/task/TaskProjectApplicationTests.java
@@ -6,6 +6,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.util.TreeMap;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.jupiter.api.Test;
@@ -29,6 +30,12 @@ class TaskProjectApplicationJUnitTest {
 	@Autowired
 	private MockMvc mockMvc;
 	
+	@Autowired
+	private TreeMap<String, Message> messagesById;
+	
+	@Autowired
+	private TreeMap<Long, Message> messageByReceivedTS;
+	
 	@Test
 	public void testNotFound() throws Exception {
 		System.out.println("------------------------------TEST_NOT_FOUND------------------------------");
@@ -45,6 +52,8 @@ class TaskProjectApplicationJUnitTest {
 				.andDo(print())
 				.andExpect(status().is4xxClientError())
 				.andReturn().getResponse();
+
+		messagesById.clear();
 		System.out.println("__________________________________________________________________________");
 		System.out.println("__________________________________________________________________________");
 		
@@ -70,6 +79,7 @@ class TaskProjectApplicationJUnitTest {
 		response = mockMvc.perform(get("/api/message")).andDo(print()).andExpect(status().is2xxSuccessful())
 		.andReturn().getResponse();
 		
+		messagesById.clear();
 		System.out.println("__________________________________________________________________________");
 		System.out.println("__________________________________________________________________________");
 	
@@ -112,7 +122,8 @@ class TaskProjectApplicationJUnitTest {
 		TimeUnit.SECONDS.sleep(1);
 		response = mockMvc.perform(get("/api/message")).andDo(print()).andExpect(status().is5xxServerError())
 		.andReturn().getResponse();
-
+		
+		messagesById.clear();
 		System.out.println("__________________________________________________________________________");
 		System.out.println("__________________________________________________________________________");
 		
@@ -161,6 +172,8 @@ class TaskProjectApplicationJUnitTest {
 		TimeUnit.SECONDS.sleep(2);
 		
 		response = mockMvc.perform(get("/api/message")).andDo(print()).andExpect(status().is2xxSuccessful()).andReturn().getResponse();
+		
+		messagesById.clear();
 		System.out.println("__________________________________________________________________________");
 		System.out.println("__________________________________________________________________________");
 	
